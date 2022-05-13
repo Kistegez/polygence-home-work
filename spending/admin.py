53113b8f77752effecd57c0cd5d6ee79a9376e06
kistegez <tegeza@gmail.com>
2022-05-13 21:36:52 +0200

    Feaure - the model registered

--- spending/admin.py
+++ spending/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from spending.models import Spending
+
+class SpendingAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Spending, SpendingAdmin)
