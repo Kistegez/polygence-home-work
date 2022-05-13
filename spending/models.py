d19781225775b502f813fd54967c169262136910
kistegez <tegeza@gmail.com>
2022-05-13 21:33:23 +0200

    Feature - spending model created

--- spending/models.py
+++ spending/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Spending (models.Model):
+    spent_at = models.DateTimeField()
+    amount = models.FloatField()
+    currency = models.TextField()
+    description = models.TextField()
