9925e120716ea7ccdb9ddc81079de8ee2c93692a
kistegez <tegeza@gmail.com>
2022-05-13 21:34:55 +0200

    Fix - url name changed

--- spendingexercise/urls.py
+++ spendingexercise/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('spendings/', include('spending.urls'))
+    path('', include('spending.urls'))
 ]
