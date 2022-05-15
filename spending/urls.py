738421fc3ffdb1cd3e6161db721d75eaa89c0266
kistegez <tegeza@gmail.com>
2022-05-15 20:34:09 +0200

    Feature - filtering url added

--- spending/urls.py
+++ spending/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from spending import views
 
 urlpatterns = [
     path('spendings/', views.spending_list),
+    path('spendings/<filter>/', views.spending_list_filter),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
