cf1aa1f2117c01d37c76916d1206032a45aea5d4
kistegez <tegeza@gmail.com>
2022-05-16 00:02:38 +0200

    Fix - get the date by default

--- spending/models.py
+++ spending/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Spending (models.Model):
-    spent_at = models.DateTimeField()
+    spent_at = models.DateTimeField(auto_now_add=True, blank=True)
     amount = models.FloatField()
     currency = models.TextField()
     description = models.TextField()
