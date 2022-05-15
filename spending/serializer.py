c49444412d2b4606662ab3dcaee09fb8f1a812cd
kistegez <tegeza@gmail.com>
2022-05-15 19:52:27 +0200

    Fix - the prular name to singular

--- spending/serializer.py
+++ spending/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from spending.models import Spending
 
-class SpendingSerializers(serializers.ModelSerializer):
+class SpendingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Spending
         fields = '__all__'
