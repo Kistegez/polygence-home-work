f2a3af3f562b6a72007a68d4a6275f726eb4b82c
kistegez <tegeza@gmail.com>
2022-05-15 20:33:49 +0200

    Feature - api requests in view

--- spending/views.py
+++ spending/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
-# request handler
+from spending.serializer import SpendingSerializer
+from .models import Spending
 
 
-def list_all_spendings():
-    return None
+@api_view(['GET', 'POST'])
+def spending_list(request, format=None):
+    if request.method == 'GET':
+        spendings = Spending.objects.all()
+        serializer = SpendingSerializer(spendings, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SpendingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+
+@api_view(['GET'])
+def spending_list_filter(request, filter, format=None):
+    try:
+        spending = Spending.objects.get(currency=filter)
+    except Spending.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = SpendingSerializer(spending)
+    return Response(serializer.data)
